cd944140d08f62dd75120ce2f94a0867e5808b6a
JulioCesar-A <juliocesar02e@gmail.com>
2025-02-17 22:45:54 -0300

    feat: Atualização na conexão com banco de dados

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import sys
 import os
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -7,14 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()  # Carrega variáveis de ambiente do .env
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL_TEST = os.getenv("DATABASE_URL_TEST")
 
+# Verifica se está rodando testes
+if "pytest" in sys.modules:
+    DATABASE_URL = DATABASE_URL_TEST  # Usa o banco de testes
 
-
-try:
-    engine = create_engine(DATABASE_URL, echo=True)
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT sqlite_version();"))
-except Exception as e:
-    print(f"Erro ao conectar: {e}")
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
